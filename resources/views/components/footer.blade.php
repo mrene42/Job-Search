37b43a8c4e3d63efbb981383c258fe0a2b595539
mrene42 <mariarenebarco.969@gmail.com>
2024-11-27 11:37:50 +0100

    feat: add estructure in footer

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,9 @@
+<footer>
+    <div>
+           <p>Copyright@rene</p>
+    </div>
+    <div>
+        <a href="https://github.com/mrene42"><img src="{{ asset('img/logoNetworking/logoGitHub.png') }}" alt="GitHub logo" width="50px"></a>
+        <a href="https://linkedin.com/"><img src="{{ asset('img/logoNetworking/logoLinkedin.png') }}" alt="Linkedin logo" width="50px"></a>
+    </div>
+</footer>
