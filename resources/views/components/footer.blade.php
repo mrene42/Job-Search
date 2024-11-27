34da4996d6cf9612b835f053bf7505f7f3a76af7
mrene42 <mariarenebarco.969@gmail.com>
2024-11-27 15:41:19 +0100

    feat: style in footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,9 +1,9 @@
 <footer>
-    <div>
+    <div class="textCopyright">
            <p>Copyright@rene</p>
     </div>
-    <div>
-        <a href="https://github.com/mrene42"><img src="{{ asset('img/logoNetworking/logoGitHub.png') }}" alt="GitHub logo" width="50px"></a>
-        <a href="https://linkedin.com/"><img src="{{ asset('img/logoNetworking/logoLinkedin.png') }}" alt="Linkedin logo" width="50px"></a>
+    <div class="Networking">
+        <a class="logoNetworking" href="https://github.com/mrene42"><img src="{{ asset('img/logoNetworking/logoGitHub.png') }}" alt="GitHub logo" width="50px"></a>
+        <a class="logoNetworking" href="https://linkedin.com/"><img src="{{ asset('img/logoNetworking/logoLinkedin.png') }}" alt="Linkedin logo" width="50px"></a>
     </div>
 </footer>
