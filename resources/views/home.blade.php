5c360ef192a409f8c97e13cc09922afc3b72b619
mrene42 <mariarenebarco.969@gmail.com>
2024-11-27 02:26:11 +0100

    feat: add estrcture in "app.blade.php" and "home.blade.php" of layouts

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,3 +1,14 @@
-<div>
-    <!-- Always remember that you are absolutely unique. Just like everyone else. - Margaret Mead -->
-</div>
+@extends('layouts.app')
+@section('content')
+    @foreach ($jobs as $job)
+    <div class="card" id = "jobsCard" style="width: 18rem;">
+        <img  id = "companyImage" src="..." class="card-img-top" alt="{{$jobs->job}}">
+        <div class="card-body">
+            <h5 class="card-title"> {{$job->offer}} </h5>
+            <h6 class="card-title"> {{$job->company}} </h6>
+            <p class="card-text"> {{$job->description}}... </p>
+            <p class="card-text"> {{$job->status}} </p>
+        </div>
+    </div>
+    @endforeach
+@endsection
