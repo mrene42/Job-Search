8cd0dd8d0edea0241a205c86c97bfb983a9576b5
mrene42 <mariarenebarco.969@gmail.com>
2024-12-02 12:07:53 +0100

    feat: all jobs in json file

--- app/Http/Controllers/Api/jobController.php
+++ app/Http/Controllers/Api/jobController.php
@@ -14,7 +14,7 @@ class JobController extends Controller
      */
     public function index()
     {
-        $jobs = Job::all();
+        $jobs = Job::get();
         return response()->json($jobs,200);
     }
 
