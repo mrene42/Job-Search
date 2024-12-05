40c7d94c713217f1142f165e720e1e375a96b22c
mrene42 <mariarenebarco.969@gmail.com>
2024-12-05 16:38:45 +0100

    fix: Api test "JobTest" in C of CRUD  (text in status)

--- tests/Feature/Api/JobTest.php
+++ tests/Feature/Api/JobTest.php
@@ -43,14 +43,14 @@ class JobTest extends TestCase
             'offer' => 'Project manager',
             'company' => 'QuentalTech',
             'description' => 'Gestor de equipos y proyectos',
-            'status' => 'In Progress'
+            'status' => 'In progress'
         ]); 
 
         $response = $this->get(route('apiHome'));
          $data1 = ['offer' => "Project manager"];
          $data2 = ['company' => "QuentalTech"];
          $data3 = ['description' => "Gestor de equipos y proyectos"];
-         $data4 = ['status' => "In Progress"];
+         $data4 = ['status' => "In progress"];
         $response->assertStatus(200)
                  ->assertJsonCount(1)
                  ->assertJsonFragment($data1)
@@ -60,50 +60,49 @@ class JobTest extends TestCase
 
         $jobs = job::factory(2)->create();
 
-    /*    $response = $this->post(route('apiCreateJob'),[
-            "offer"=> "Desarrollo Web",
-            "company" => "CIBERCI",
-            "description" => "Conocimientos tecnicos en PHP",
-            "status" => "In Progress",
+        $response = $this->post(route('apiCreateJob'),[
+            'offer' => "Desarrollo Web",
+            'company' => "CIBERCI",
+            'description' => "Conocimientos tecnicos en PHP",
+            'status' => "In progress",
         ]);
-        */
-       /* $response = $this->get(route('apiHome'));
-         $data1 = ["offer" => "Desarrollo Web"];
-         $data2 = ["company" => "CIBERCI"];
-         $data3 = ["description" => "Conocimientos tecnicos en PHP"];
-         $data4 = ["status" => "In Progress"];
+    
+        $response = $this->get(route('apiHome'));
+         $data1 = ['offer' => "Desarrollo Web"];
+         $data2 = ['company' => "CIBERCI"];
+         $data3 = ['description' => "Conocimientos tecnicos en PHP"];
+         $data4 = ['status' => "In progress"];
         $response->assertStatus(200)
                  ->assertJsonCount(4)
                  ->assertJsonFragment($data1)
                  ->assertJsonFragment($data2)
                  ->assertJsonFragment($data3)
                  ->assertJsonFragment($data4);
-                 */
     }
 
     //U de CRUD
     public function test_CheckIfCanUpdateOneJobWithJsonFile(){
         $response = $this->post(route('apiCreateJob'),[
-            "offer" => "Ingeniero Software",
-            "company" => "CIBETECH",
-            "description" => "Manejo de sistemas operativos Linux",
-            "status" => "Completed",
+            'offer' => 'Ingeniero Software',
+            'company' => 'CIBETECH',
+            'description' => 'Manejo de sistemas operativos Linux',
+            'status' => 'Completed',
         ]);
 
-        $data = ["description" => "Manejo de sistemas operativos Linux"];
+        $data = ['description' => "Manejo de sistemas operativos Linux"];
         $response = $this->get(route('apiHome'));
         $response->assertStatus(200)
                  ->assertJsonCount(1)
                  ->assertJsonFragment($data);
 
         $response = $this->put(route('apiUpdateJob', 1), [
-            "offer" => "Software Engineer",
-            "company" => "CIBETECH",
-            "description" => "Manejo de sistemas operativos Linux y PHP",
-            "status" => "Completed",
+            'offer' => "Software Engineer",
+            'company' => "CIBETECH",
+            'description' => "Manejo de sistemas operativos Linux y PHP",
+            'status' => "Completed",
         ]);
 
-        $data = ["description" => "Manejo de sistemas operativos Linux y PHP"];
+        $data = ['description' => "Manejo de sistemas operativos Linux y PHP"];
         $response = $this->get(route('apiHome'));
         $response->assertStatus(200)
                  ->assertJsonCount(1)
