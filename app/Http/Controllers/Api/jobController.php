e44c13620cb0ce2d232ff8f079cfec65a1a7f03a
mrene42 <mariarenebarco.969@gmail.com>
2024-12-09 16:47:35 +0100

    fix: mute unused functions in controllers

--- app/Http/Controllers/Api/jobController.php
+++ app/Http/Controllers/Api/jobController.php
@@ -21,10 +21,10 @@ class JobController extends Controller
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
+    /*public function create()
     {
         //
-    }
+    } */
 
     /**
      * Store a newly created resource in storage.
@@ -54,10 +54,10 @@ class JobController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    /*public function edit(string $id)
     {
         //
-    }
+    }   */
 
     /**
      * Update the specified resource in storage.
--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -19,18 +19,18 @@ class JobController extends Controller
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
+    /*public function create()
     {
         //
-    }
+    }   */
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    /*public function store(Request $request)
     {
         //
-    }
+    }   */
 
     /**
      * Display the specified resource.
@@ -44,24 +44,24 @@ class JobController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    /*public function edit(string $id)
     {
         //
-    }
+    }   */
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    /*public function update(Request $request, string $id)
     {
         //
-    }
+    }   */
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    /*public function destroy(string $id)
     {
         //
-    }
+    }   */
 }
