2f54d1f81829d7c92eb6256fd72093e188b3ab1f
mrene42 <mariarenebarco.969@gmail.com>
2024-12-10 20:02:32 +0100

    feat: add update en home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,16 +1,17 @@
 @extends('layouts.app')
 @section('content')
     <div>
-        <table class="table">
+        <table class="tableJob">
             <thead>
                 <tr>
-                    <th>#</th>
+                    <th>ID</th>
                     <th>Date</th>
+                    <th>Update</th>
                     <th>Offer</th>
                     <th>Company</th>
                     <th>Description</th>
                     <th>Status</th>
-                    <th>Info</th>
+                    <th>News</th>
                 </tr>
             </thead>
             <tbody>
@@ -18,13 +19,14 @@
                  <tr>
                      <td>{{$job->id}}</td>
                      <td>{{$job->created_at}}</td>
+                     <td>{{$job->updated_at}}</td>
                      <td>{{$job->offer}}</td>
                      <td>{{$job->company}}</td>
-                     <td>{{$job->description}}...</td>
+                     <td>{{$job->description}}</td>
                      <td>{{$job->status}}</td>
                      <td>
                         <a href="{{ route('showDetail', ['id' => $job->id]) }}">
-                            <button type="button" class="btn">Show</button>
+                            <button class="btn">Show</button>
                         </a>
                     </td>
                  </tr>
